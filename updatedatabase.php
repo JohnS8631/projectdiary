10564d0adb1ecb2a0b581638ab3e2019427223c1
nikhilkumar9764 <34518219+nikhilkumar9764@users.noreply.github.com>
2018-08-12 21:24:03 +0530

    Update updatedatabase.php

--- updatedatabase.php
+++ updatedatabase.php
@@ -1,9 +1,10 @@
+
 <?php
 session_start();
-if(array_key_exists("content",$_POST))
+if(array_key_exists("content",$_POST) && array_key_exists('emailad',$_SESSION))
 {
   include("connection.php");
-  $query="UPDATE  PROJECTDIARY SET diary_contents='".mysqli_real_escape_string($link,$_POST['content'])."' WHERE id=11";
+  $query="UPDATE  PROJECTDIARY SET diary_contents='".mysqli_real_escape_string($link,$_POST['content'])."' WHERE email='".mysqli_real_escape_string($link,$_SESSION['emailad'])."'";
   mysqli_query($link,$query);
 }
 ?>
