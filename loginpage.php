6cbad5a4821fa7cea5c583d805efbca164e84358
nikhilkumar9764 <34518219+nikhilkumar9764@users.noreply.github.com>
2018-08-12 21:22:15 +0530

    Update loginpage.php

--- loginpage.php
+++ loginpage.php
@@ -1,17 +1,13 @@
 <?php
 session_start();
 $dcontent="";
-if(array_key_exists('id',$_COOKIE))
+if(array_key_exists('emailad',$_SESSION))
 {
-  $_SESSION['id']=$_COOKIE['id'];
-}
-if(array_key_exists('id',$_SESSION))
-{
-  echo $_SESSION['id'];
   echo "<p id='kl'>Logged In!!<a href='index.php?logout=1'><button id='lop' class='btn btn-success' type='submit'>Log Out</button></a></p>";
   include("connection.php");
-  $query="SELECT diary_contents FROM PROJECTDIARY WHERE id=11";
-  $row=mysqli_fetch_array(mysqli_query($link,$query));
+  $query="SELECT diary_contents FROM PROJECTDIARY WHERE email='".mysqli_real_escape_string($link,$_SESSION['emailad'])."'";
+  $result=mysqli_query($link,$query);
+  $row=mysqli_fetch_array($result);
   $dcontent=$row['diary_contents'];
 }
 else
